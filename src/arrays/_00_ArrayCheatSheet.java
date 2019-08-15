355473e3b846d4ea01032c16e19eb41729db6ef2
league <league@imac-22.attlocal.net>
2019-08-14 18:28:07 -0700

    Array ip

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,34 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] r = {"Hello", "I", "Speak", "Mild", "Portuguese"};
 		//2. print the third element in the array
-		
+		System.out.println(r[3]);
 		//3. set the third element to a different value
-		
+		r[3] = "Talk";
 		//4. print the third element again
-		
+		System.out.println(r[3]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
-		
+		for (int h = 0; h < 6; h++) {
+			System.out.println(r[h]);
+		}
 		//6. make an array of 50 integers
-		
+			int[] p = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+			for (int k = 0; k < 50; k++) {
+				Random n = new Random();
+				int[] p = {n.nextInt(51)};
+			}
 		//8. without printing the entire array, print only the smallest number in the array
-		
+		System.out.println();
 		//9 print the entire array to see if step 8 was correct
-		
+		System.out.println(p);
 		//10. print the largest number in the array.
+		System.out.println(p[50]);
 	}
 }
